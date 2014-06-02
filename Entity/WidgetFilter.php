776c8d017309acb5dcaa1861cf974ddb63e1bd7c
Thomas Beaujean <thomas@appventus.com>
2014-06-02 09:43:38 +0200

    [VICMS-180] refactor victoire

--- Entity/WidgetFilter.php
+++ Entity/WidgetFilter.php
@@ -12,8 +12,6 @@ use Victoire\Bundle\CoreBundle\Entity\Widget;
  */
 class WidgetFilter extends Widget
 {
-    use \Victoire\Bundle\CoreBundle\Entity\Traits\WidgetTrait;
-
     /**
      * @var string
      *
